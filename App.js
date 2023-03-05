10b2e38ad8219cf9ecf4fd9da272e5e781256ef4
retz8 <ekkicb71@gmail.com>
2023-03-04 20:09:53 -0500

    2 added

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>git test</Text>
+      <Text>git test2</Text>
       <StatusBar style="auto" />
     </View>
   );
