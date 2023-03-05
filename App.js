895debdab86435b7585e6cdb8f09908066a7ba5c
retz8 <ekkicb71@gmail.com>
2023-03-04 19:59:58 -0500

    git test added

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World! Dev</Text>
+      <Text>git test</Text>
       <StatusBar style="auto" />
     </View>
   );
